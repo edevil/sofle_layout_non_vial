f3cc95bdb0881c293d9db309fd9f5d779f648ac0
André Cruz <acruz@cloudflare.com>
2025-03-19 17:57:38 +0000

    Simplified layout

--- keymap.c
+++ keymap.c
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_NONUS_HASH,
     KC_NONUS_BACKSLASH, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H, RSFT_T(KC_J), LGUI_T(KC_K), LALT_T(KC_L), RCTL_T(KC_RBRC), KC_QUOT,
     SC_LSPO,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,    KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_LBRC, SC_RSPC,
-    KC_DELETE, KC_LCTL, KC_LALT, LGUI_T(KC_KP_ASTERISK), LT(MDIA, KC_SPACE),       LT(MDIA, KC_ENTER), LCTL_T(KC_SLASH), KC_EQUAL, KC_KP_SLASH, KC_MINUS
+    KC_DELETE,    KC_LCTL, KC_LALT, KC_LGUI, LT(MDIA, KC_SPACE),                      KC_ENTER, KC_SLASH, KC_EQUAL, KC_KP_SLASH, KC_MINUS
 ),
 [MDIA] = LAYOUT_split_4x6_5(
     _______,_______,_______,_______,_______,_______,                              KC_PGUP,   LCTL(KC_A),     KC_UP,      LCTL(KC_E),  _______,LSFT(KC_MINUS),
     RM_TOGG,_______,_______,_______,_______,_______,                              KC_PGDN,      KC_LEFT,      KC_DOWN,     KC_RIGHT,  _______,_______,
-    RM_NEXT,_______,_______,_______,_______,LALT(KC_8),                           LSA(KC_8),  0xc50, LGUI(KC_NONUS_BACKSLASH),  0xc4f,_______,_______,
-    RM_HUEU,_______,_______,_______,_______,LALT(KC_9),   _______,     _______,   LSA(KC_9),_______,_______,_______,_______,_______,
+    RM_NEXT,_______,_______,_______,_______,_______,                              _______,  0xc50, LGUI(KC_NONUS_BACKSLASH),  0xc4f,_______,_______,
+    RM_HUEU,_______,_______,_______,_______,_______,   _______,     _______,      _______,_______,_______,_______,_______,_______,
                     _______,_______,_______,_______,      _______,     _______,   _______,_______,_______,_______
 )
 };
