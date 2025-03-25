4c65b35ea238f4d89dcfc20c76c4d6f344932579
André Cruz <acruz@cloudflare.com>
2025-03-25 15:36:08 +0000

    Removed space cadet...

--- keymap.c
+++ keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_BACKSPACE,
     KC_TAB,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_NONUS_HASH,
     KC_NONUS_BACKSLASH, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H, RSFT_T(KC_J), LGUI_T(KC_K), LALT_T(KC_L), RCTL_T(KC_RBRC), KC_QUOT,
-    SC_LSPO,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,    KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_LBRC, SC_RSPC,
+    KC_LSFT,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,    KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_LBRC, KC_RSFT,
     KC_DELETE,    KC_LCTL, KC_LALT, KC_LGUI, LT(MDIA, KC_SPACE),                      KC_ENTER, KC_SLASH, KC_EQUAL, KC_KP_SLASH, KC_MINUS
 ),
 [MDIA] = LAYOUT_split_4x6_5(
